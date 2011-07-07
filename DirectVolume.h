58ea4ccab73b756369e1b97f40fe81471ac2e8dd
guoyin.chen <y>
2011-07-07 12:50:58 +0800

    Extend the partition number from 4 to 6
    Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>

--- DirectVolume.h
+++ DirectVolume.h
@@ -27,7 +27,7 @@ typedef android::List<char *> PathCollection;
 
 class DirectVolume : public Volume {
 public:
-    static const int MAX_PARTITIONS = 4;
+    static const int MAX_PARTITIONS = 6;
 protected:
     PathCollection *mPaths;
     int            mDiskMajor;
