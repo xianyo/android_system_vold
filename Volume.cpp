e0fcbea72e8f00922183efa4b368964bbf0df30a
Xinyu Chen <xinyu.chen@freescale.com>
2012-11-08 11:46:30 +0800

    ENGR00232884 vold: add NTFS filesystem support
    Add fuse based NTFS-3g filesystem support.
    The fsck_msdos will not break the ntfs, so
    we take the chance after msdos fsck failure
    to check the NTFS existed or not.
    
    Change-Id: I10b3801eb8fb49eb9f6d0e7ee992c64a417d128a
    Signed-off-by: Xinyu Chen <xinyu.chen@freescale.com>

--- Volume.cpp
+++ Volume.cpp
@@ -44,6 +44,7 @@
 #include "VolumeManager.h"
 #include "ResponseCode.h"
 #include "Fat.h"
+#include "Ntfs.h"
 #include "Process.h"
 #include "cryptfs.h"
 
@@ -398,9 +399,18 @@ int Volume::mountVol() {
         errno = 0;
         setState(Volume::State_Checking);
 
+        int ntfs = 0;
         if (Fat::check(devicePath)) {
             if (errno == ENODATA) {
                 SLOGW("%s does not contain a FAT filesystem\n", devicePath);
+                /* try the NTFS filesystem */
+                if (!Ntfs::check(devicePath)) {
+                    ntfs = 1;
+                    SLOGI("%s contain a NTFS filesystem\n", devicePath);
+                    goto mnt;
+                } else
+                    SLOGW("%s does not contain a NTFS filesystem\n", devicePath);
+
                 continue;
             }
             errno = EIO;
@@ -410,6 +420,7 @@ int Volume::mountVol() {
             return -1;
         }
 
+mnt:
         /*
          * Mount the device on our internal staging mountpoint so we can
          * muck with it before exposing it to non priviledged users.
@@ -425,7 +436,13 @@ int Volume::mountVol() {
             // For secondary external storage we keep things locked up.
             gid = AID_MEDIA_RW;
         }
-        if (Fat::doMount(devicePath, "/mnt/secure/staging", false, false, false,
+        if (ntfs) {
+            if (Ntfs::doMount(devicePath, "/mnt/secure/staging", false, false, false,
+                    AID_SYSTEM, gid, 0702, true)) {
+                SLOGE("%s failed to mount via NTFS (%s)\n", devicePath, strerror(errno));
+                continue;
+            }
+        } else if (Fat::doMount(devicePath, "/mnt/secure/staging", false, false, false,
                 AID_SYSTEM, gid, 0702, true)) {
             SLOGE("%s failed to mount via VFAT (%s)\n", devicePath, strerror(errno));
             continue;
